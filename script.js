476c990e5400a583dce8b0b53508151b679bcf15
GermanSTr <gershev1994@gmail.com>
2023-09-02 14:46:51 +0300

    123

---
+++ script.js
@@ -0,0 +1,9 @@
+const emptyArray = [10];
+// emptyArray.push(1)
+// console.log(emptyArray)
+// const length1 = emptyArray.push(1)
+// console.log(length1) // 3
+// const length2 = emptyArray.push(2);
+emptyArray.unshift(3);
+console.log(emptyArray);
+// console.log(length2);
